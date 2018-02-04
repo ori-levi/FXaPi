57bdee96384560035faa70b95a1d184838255355
Ori Levi <ori1991@gmail.com>
2018-02-05 01:33:51 +0200

    reformat helpers

--- fxplib/helpers.py
+++ fxplib/helpers.py
@@ -1,15 +1,13 @@
-from urllib.parse import urlparse
 import requests
-def urlAlive(url):
-	min_attr = ('scheme' , 'netloc')
-	try:
-		result = urlparse(url)
-		if all([result.scheme, result.netloc]):
-			if requests.head(url).status_code == 200:
-				return True
-			else:
-				return True
-		else:
-			return False
-	except:
-		return False
+
+from urllib.parse import urlparse
+
+
+def url_alive(url):
+    try:
+        result = urlparse(url)
+        if all([result.scheme, result.netloc]):
+            return requests.head(url).status_code == 200
+        return False
+    except:
+        return False
